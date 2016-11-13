3d0bf819f7f719bcade096fd4a14ec80d88b5a95
Daniel Berry <danielberry@Daniels-MacBook-Pro.local>
2016-11-13 11:12:10 -0500

    code to read in raw vacant buildings

--- ETL.r
+++ ETL.r
@@ -3,4 +3,31 @@
 ## Description: Code to transform data ##
 #########################################
 
-vacant_raw <- read.csv()
+getwd()
+
+vacant_raw <- read.csv('311_Service_Requests_-_Vacant_and_Abandoned_Buildings_Reported_-_Map.csv',
+                       stringsAsFactors = FALSE,
+                       skip = 1)
+
+names(vacant_raw) <- c('Type',
+                       'ID',
+                       'Date_Recieved',
+                       'Lot_Location',
+                       'Open',
+                       'Dangerous',
+                       'Entry',
+                       'Vacant',
+                       'Fire',
+                       'Users',
+                       'Address_Number',
+                       'Address_Direction',
+                       'Address_Street',
+                       'Address_Suffic',
+                       'Zip',
+                       'X_Coordinate',
+                       'Y_Coordinate',
+                       'Latitude',
+                       'Longitude',
+                       'Location_string')
+
+
