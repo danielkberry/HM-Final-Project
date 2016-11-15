ae54a550944a8399635c5d40e47047c61fa7deee
Daniel Berry <danielberry@Daniels-MacBook-Pro.local>
2016-11-15 08:16:16 -0500

    Added repo

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@
 sudo yum update
 sudo yum install R
 
-R -e "install.packages('sp')"
-R -e "install.packages('magrittr')"
-R -e "install.packages('stringr')"
+R -e "install.packages('sp', repos = 'https://cloud.r-project.org/')"
+R -e "install.packages('magrittr', repos = 'https://cloud.r-project.org/')"
+R -e "install.packages('stringr', repos = 'https://cloud.r-project.org/')"
